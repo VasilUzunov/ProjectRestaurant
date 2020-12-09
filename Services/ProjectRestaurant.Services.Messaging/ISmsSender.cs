4f8ecddea451fed9880422b007f1e30d4d7ef363
Vasil Uzunov <vasil6062@gmail.com>
2020-12-09 19:50:54 +0200

    add ISmsSender

---
+++ Services/ProjectRestaurant.Services.Messaging/ISmsSender.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectRestaurant.Services.Messaging
+{
+    using System.Threading.Tasks;
+
+    public interface ISmsSender
+    {
+        Task SendEmailAsync(
+            string to,
+            string message);
+    }
+}
