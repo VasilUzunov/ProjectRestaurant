c0ff6a2bfab6ff5cc3361235ffdae9ebe2a0ece4
Vasil Uzunov <vasil6062@gmail.com>
2020-12-16 13:36:14 +0200

    Add some tests

---
+++ Tests/ProjectRestaurant.Services.Data.Tests/EventServiceTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using ProjectRestaurant.Data.Common.Repositories;
+using ProjectRestaurant.Data.Models;
+using ProjectRestaurant.Data.Repositories;
+using ProjectRestaurant.Services.Data.Tests.Common;
+using ProjectRestaurant.Services.Mapping;
+using ProjectRestaurant.Web.ViewModels;
+using ProjectRestaurant.Web.ViewModels.Administration;
+using ProjectRestaurant.Web.ViewModels.Event;
+using Xunit;
+
+namespace ProjectRestaurant.Services.Data.Tests
+{
+    public class EventServiceTests
+    {
+        private readonly IDeletableEntityRepository<Event> eventRepository;
+        private readonly IEventService eventService;
+
+        public EventServiceTests()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+
+            var context = ApplicationDbContextInMemoryFactory.InitializeContext();
+            this.eventRepository = new EfDeletableEntityRepository<Event>(context);
+            this.eventService = new EventService(this.eventRepository);
+        }
+
+        [Fact]
+        public async Task CreateEventAsyncShouldWorkAsIsExpected()
+        {
+            const string name = "Birthday";
+            const string description = "oneoneoneone oneoneoneone oneoneoneone oneoneoneone";
+            const double price = 12.50;
+            const string imagePath = "123456789.png";
+            const string contentMessage = "Hello World from a Fake File";
+
+            var fileMock = new Mock<IFormFile>();
+            var content = contentMessage;
+            var fileName = imagePath;
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+            ms.Position = 0;
+            fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
+            fileMock.Setup(_ => _.FileName).Returns(fileName);
+            fileMock.Setup(_ => _.Length).Returns(ms.Length);
+            var file = fileMock.Object;
+
+            var model = new EventInputModel()
+            {
+                Description = description,
+                Name = name,
+                Price = price,
+                Image = file,
+            };
+
+            var totalObjectsBeforeAdding = this.eventRepository.AllAsNoTracking().Count();
+
+            await this.eventService.CreateAsyncEvent(model, "wwwroot/images");
+
+            var totalObjectsAfterAdding = this.eventRepository.AllAsNoTracking().Count();
+
+            var actualObject = await this.eventRepository.GetByIdWithDeletedAsync(1);
+            Assert.NotNull(actualObject);
+
+            Assert.Equal(name, actualObject.Name);
+            Assert.Equal(description, actualObject.Description);
+            Assert.Equal(price, actualObject.Price);
+            Assert.Equal("png", actualObject.EventImage.Extension);
+
+            Assert.Equal(0, totalObjectsBeforeAdding);
+            Assert.Equal(1, totalObjectsAfterAdding);
+        }
+
+        [Fact]
+        public async Task CreateEventAsyncShouldCheckCategory()
+        {
+            const string name = "Birthday";
+            const string description = "oneoneoneone oneoneoneone oneoneoneone oneoneoneone";
+            const double price = 12.50;
+            const string imagePath = "123456789.pdf";
+            const string contentMessage = "Hello World from a Fake File";
+
+            var fileMock = new Mock<IFormFile>();
+            var content = contentMessage;
+            var fileName = imagePath;
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+            ms.Position = 0;
+            fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
+            fileMock.Setup(_ => _.FileName).Returns(fileName);
+            fileMock.Setup(_ => _.Length).Returns(ms.Length);
+            var file = fileMock.Object;
+
+            var model = new EventInputModel()
+            {
+                Description = description,
+                Name = name,
+                Price = price,
+                Image = file,
+            };
+
+            var totalObjectsBeforeAdding = this.eventRepository.AllAsNoTracking().Count();
+
+            var ex = this.eventService.CreateAsyncEvent(model, "wwwroot/images");
+
+            Assert.Equal("Invalid image extension pdf", ex.Exception.InnerException.Message);
+
+            var totalObjectsAfterAdding = this.eventRepository.AllAsNoTracking().Count();
+
+            Assert.Equal(totalObjectsBeforeAdding, totalObjectsAfterAdding);
+        }
+
+        [Fact]
+        public void GetAllShouldWorkAsIsExpected()
+        {
+            var mockEventRepository = new Mock<IDeletableEntityRepository<Event>>();
+
+            mockEventRepository.Setup(x => x.AllAsNoTracking())
+                .Returns(new List<Event>()
+                {
+                    new Event()
+                    {
+                        Name = "Birthday",
+                        Price = 12.50,
+                        EventImage = new EventImage{Id = "klsajldksadklsadsdlkjsadklj", Extension = "png" },
+                        Description = "oneoneoneone oneoneoneone oneoneoneone oneoneoneone",
+                    },
+                    new Event()
+                    {
+                        Name = "New Year",
+                        Price = 15.50,
+                        EventImage = new EventImage{Id = "klsajldksadklsadsdlkjsadklj", Extension = "png" },
+                        Description = "oneoneoneone oneoneoneone oneoneoneone oneoneoneone",
+                    },
+                }.AsQueryable());
+
+            var eventService = new EventService(mockEventRepository.Object);
+
+            var allItems = eventService.GetAll<EventViewModel>();
+
+            Assert.Equal(2, allItems.Count());
+
+            foreach (var item in allItems)
+            {
+                Assert.Equal("oneoneoneone oneoneoneone oneoneoneone oneoneoneone", item.Description);
+                Assert.Equal("/images/events/klsajldksadklsadsdlkjsadklj.png", item.ImageUrl);
+                Assert.Equal("Birthday", item.Name);
+                Assert.Equal(12.50, item.Price);
+                break;
+            }
+        }
+    }
+}
---
+++ Tests/ProjectRestaurant.Services.Data.Tests/ReservationServiceTests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+
+namespace ProjectRestaurant.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Moq;
+    using ProjectRestaurant.Data.Common.Repositories;
+    using ProjectRestaurant.Data.Models;
+    using ProjectRestaurant.Data.Repositories;
+    using ProjectRestaurant.Services.Data.Tests.Common;
+    using ProjectRestaurant.Services.Mapping;
+    using ProjectRestaurant.Web.ViewModels;
+    using ProjectRestaurant.Web.ViewModels.Administration;
+    using ProjectRestaurant.Web.ViewModels.Reservation;
+    using Xunit;
+
+    public class ReservationServiceTests
+    {
+        private readonly IDeletableEntityRepository<Reservation> reservationRepository;
+        private readonly IDeletableEntityRepository<Table> tableRepository;
+        private readonly IDeletableEntityRepository<ApplicationUser> userRepository;
+        private readonly IReservationsService reservationsService;
+        private readonly ITableService tableService;
+
+        public ReservationServiceTests()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+
+            var context = ApplicationDbContextInMemoryFactory.InitializeContext();
+            this.reservationRepository = new EfDeletableEntityRepository<Reservation>(context);
+            this.tableRepository = new EfDeletableEntityRepository<Table>(context);
+            this.userRepository = new EfDeletableEntityRepository<ApplicationUser>(context);
+            this.reservationsService = new ReservationsService(this.reservationRepository, this.tableRepository, this.userRepository);
+            this.tableService = new TableService(this.tableRepository);
+        }
+
+        [Fact]
+        public async Task CreateReservationAsyncShouldWorkAsIsExpected()
+        {
+            const string message = "Beerbeerrrrrrrjkhsajkdskdasdalkjsasadjkldsa";
+            const string date = "12/12/2020";
+            const string hour = "12";
+            const string minutes = "12";
+            const int table = 3;
+            const int numberOfPeople = 3;
+            const string userId = "68a7b421-feca-40b2-bece-ecaf81f6ebb8";
+
+            var model = new ReservationInputModel()
+            {
+                Message = message,
+                Date = date,
+                Hour = hour,
+                Table = table,
+                Minutes = minutes,
+                NumberOfPeople = numberOfPeople,
+            };
+
+            await this.tableService.CreateAsyncTable(new TableInputModel
+            {
+                ShapeOfTable = "oval",
+                NumberOfSeats = numberOfPeople,
+                NumberOfTable = table,
+            });
+
+            var totalObjectsBeforeAdding = this.reservationRepository.AllAsNoTracking().Count();
+
+            await this.reservationsService.CreateAsyncReservation(model, userId);
+
+            var totalObjectsAfterAdding = this.reservationRepository.AllAsNoTracking().Count();
+
+            var actualObject = await this.reservationRepository.GetByIdWithDeletedAsync(1);
+            Assert.NotNull(actualObject);
+
+            Assert.Equal(message, actualObject.Message);
+            Assert.Equal(userId, actualObject.UserId);
+            Assert.Equal(table, actualObject.Table.TableNumber);
+            Assert.Equal(numberOfPeople, actualObject.Table.NumberOfSeats);
+            Assert.Equal($"{date} {hour}:{minutes}:00 PM", actualObject.DateAndTimeOfReservation.ToString());
+
+            Assert.Equal(0, totalObjectsBeforeAdding);
+            Assert.Equal(1, totalObjectsAfterAdding);
+        }
+
+        //[Fact]
+        //public void GetAllShouldWorkAsIsExpected()
+        //{
+        //    var mockReservationRepository = new Mock<IDeletableEntityRepository<Reservation>>();
+        //    var mockTableRepository = new Mock<IDeletableEntityRepository<Table>>();
+        //    var mockUserRepository = new Mock<IDeletableEntityRepository<ApplicationUser>>();
+
+        //    mockTableRepository.Setup(x => x.AllAsNoTracking())
+        //        .Returns(new List<Table>()
+        //        {
+        //            new Table()
+        //            {
+        //                NumberOfSeats = 3,
+        //                ShapeOfTable = "oval",
+        //                TableNumber = 3,
+        //            },
+        //        }.AsQueryable());
+
+        //    mockUserRepository.Setup(x => x.AllAsNoTracking())
+        //        .Returns(new List<ApplicationUser>()
+        //        {
+        //            new ApplicationUser()
+        //            {
+        //                Address = "nsakjsalkjalkjAJASlkajSLKASjaslKJSAklas",
+        //                Email = "vasil@abv.bg",
+        //                FirstName = "Vasil",
+        //                PhoneNumber = "0877980616",
+        //                LastName = "Uzunov",
+        //                PasswordHash = "AQAAAAEAACcQAAAAELHKxwpRjNH7bxS7xiHAhy95SNG/7le/06QkI0TM8kmjFGUlueU+iS+TnCaogfjRzw==",
+        //            },
+        //        }.AsQueryable());
+
+        //    mockReservationRepository.Setup(x => x.AllAsNoTracking())
+        //        .Returns(new List<Reservation>()
+        //        {
+        //            new Reservation()
+        //            {
+        //                UserId = mockUserRepository.Object.AllAsNoTrackingWithDeleted().First().Id,
+        //                Message = "kjsakjhdsjkhsakjsahsadkjhsakhjsadk",
+        //                TableId = mockTableRepository.Object.AllAsNoTrackingWithDeleted().First().Id,
+        //                DateAndTimeOfReservation = DateTime.Now,
+        //            },
+        //        }.AsQueryable());
+
+        //    var reservationService = new ReservationsService (mockReservationRepository.Object, mockTableRepository.Object, mockUserRepository.Object);
+
+        //    var allItems = reservationService.GetAll<ReservationViewModel>();
+
+        //    Assert.Equal(2, allItems.Count());
+
+        //    foreach (var item in allItems)
+        //    {
+        //        Assert.Equal("oneoneoneone oneoneoneone oneoneoneone oneoneoneone", item.Description);
+        //        Assert.Equal("/images/menu/klsajldksadklsadsdlkjsadklj.png", item.ImageUrl);
+        //        Assert.Equal("Beer", item.Name);
+        //        Assert.Equal(500, item.PortionWeight);
+        //        Assert.Equal(12.50, item.Price);
+        //        Assert.Equal("Drinks", item.CategoryName);
+        //        break;
+        //    }
+        //}
+
+        [Fact]
+        public void GetPhoneNumberShouldWorkAsIsExpected()
+        {
+            var mockReservationRepository = new Mock<IDeletableEntityRepository<Reservation>>();
+            var mockTableRepository = new Mock<IDeletableEntityRepository<Table>>();
+            var mockUserRepository = new Mock<IDeletableEntityRepository<ApplicationUser>>();
+
+            mockUserRepository.Setup(x => x.AllAsNoTracking())
+                .Returns(new List<ApplicationUser>()
+                {
+                        new ApplicationUser()
+                        {
+                            Id = "1d1a2708-fd4c-422a-bf1a-22bd1a2e55e3",
+                            Address = "nsakjsalkjalkjAJASlkajSLKASjaslKJSAklas",
+                            Email = "vasil@abv.bg",
+                            FirstName = "Vasil",
+                            PhoneNumber = "0877980616",
+                            LastName = "Uzunov",
+                            PasswordHash = "AQAAAAEAACcQAAAAELHKxwpRjNH7bxS7xiHAhy95SNG/7le/06QkI0TM8kmjFGUlueU+iS+TnCaogfjRzw==",
+                        },
+                }.AsQueryable());
+
+            var reservationService = new ReservationsService(mockReservationRepository.Object, mockTableRepository.Object, mockUserRepository.Object);
+
+            var allItems = reservationService.GetPhoneNumber("1d1a2708-fd4c-422a-bf1a-22bd1a2e55e3");
+
+            Assert.Equal(10, allItems.Length);
+            Assert.Equal("0877980616", allItems);
+        }
+    }
+}
